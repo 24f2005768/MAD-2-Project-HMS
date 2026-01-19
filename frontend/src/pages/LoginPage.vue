633a1d2aae68bf4fe9432003a436976c3246f7ae
Kriti <24f2005768@ds.study.iitm.ac.in>
2026-01-20 00:32:19 +0530

    Started building frontend

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is LoginPage</h1>
+</template>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/pages/HomePage.vue'
+import LoginPage from '@/pages/LoginPage.vue'
+import SignUpPage from '@/pages/SignUpPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {path: '/', component: HomePage},
+    {path: '/login', component: LoginPage},
+    {path: '/signup', component: SignUpPage}
+  ],
+})
+
+export default router
